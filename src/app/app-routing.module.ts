abc32dea81fa12f9b60ef141e071e4b30277c856
virajstrivedge <viraj.strivedge@gmail.com>
2023-04-14 18:01:21 +0530

    set variables in event-details.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {EventDetailsComponent} from "./pages/event-details/event-details.component";
+import {InvoiceDetailsComponent} from "./pages/invoice-details/invoice-details.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:'event-details/:id',
     component:EventDetailsComponent
+  },
+  {
+    path:'invoice-details',
+    component:InvoiceDetailsComponent
   }
 ];
 
